838621334dfefc1bcdc7f9cea63bb41a247c9952
PaoloFabris98 <f98paolo@gmail.com>
2024-11-16 16:22:12 +0100

    feat: end

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,27 +1,46 @@
 package com.ferragnez.party;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import guestClass.guestsClass;
 
 public class CheckGuest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
-                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
 
-        System.out.println("Dimmi il tuo nome per sapere se sei invitato alla festa.");
-        String guest = input.nextLine();
-        int itteration = 0;
-        Boolean finded = false;
+        ArrayList<guestsClass> guestObjects = new ArrayList<>();
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
+                "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone",
+                "Rachel Zeilic" };
+        Boolean[] invitation = { true, false, true, false, true, true, false, true, false, true, true };
 
-        while (itteration < guests.length || finded == false) {
-            if (guests[itteration].equals(guest)) {
-                System.out.println("Sei invitato alla festa.");
-                break;
-            } else {
-                System.out.println("Non sei invitato alla festa.");
-            }
-            itteration++;
+        for (int i = 0; i < guests.length; i++) {
+            guestObjects.add(new guestsClass(guests[i], invitation[i]));
         }
+
+        System.out.println("\nDimmi il tuo nome per sapere se sei invitato alla festa.");
+        String guestName = input.nextLine();
+
+        index = 0;
+        boolean found = false;
+        while (index < guestObjects.size()) {
+            guestsClass currentGuest = guestObjects.get(index);
+            if (currentGuest.name.equalsIgnoreCase(guestName)) {
+                if (currentGuest.invitation) {
+                    System.out.println("Sei invitato alla festa!");
+                } else {
+                    System.out.println("Non sei invitato alla festa.");
+                }
+                found = true;
+                break;
+            }
+            index++;
+        }
+
+        if (!found) {
+            System.out.println("Non sei sulla lista degli ospiti.");
+        }
+
         input.close();
     }
 }
---
+++ src/guestClass/guestsClass.java
@@ -0,0 +1,12 @@
+package guestClass;
+
+public class guestsClass {
+    public String name;
+    public Boolean invitation;
+
+    public guestsClass(String nome, Boolean invitations) {
+        this.name = nome;
+        this.invitation = invitations;
+    }
+
+}
